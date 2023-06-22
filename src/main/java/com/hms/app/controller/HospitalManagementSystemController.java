283fc6f090f53496d98973ee8b90321f3ede5c1f
SaiKiranNandipati <nandipatisaikiran3761@gmail.com>
2023-06-21 22:38:48 -0600

    adding save patient  functionality in HospitalManagementSystemController.java

--- src/main/java/com/hms/app/controller/HospitalManagementSystemController.java
+++ src/main/java/com/hms/app/controller/HospitalManagementSystemController.java
@@ -40,10 +40,37 @@ public class HospitalManagementSystemController {
 	}
 
 	@GetMapping("/login")
-	public String getLoginPage(Model model,  HttpSession session, HttpServletRequest request)
-	{	
+	public String getLoginPage(Model model, HttpSession session, HttpServletRequest request) {
 		request.getSession().invalidate();
 		Patient patient = new Patient();
 		model.addAttribute("patient", patient);
 		return "home/login";
+	}
+
+	@PostMapping("/savePatient")
+	public String savePatient(@ModelAttribute("patient") Patient patient,Model model) {
+		System.out.println("save===patient");
+
+		Patient existingPatient = patientService.findPatient(patient.getEmail());
+
+		if (existingPatient != null) {
+			model.addAttribute("errormsg", "Email already exists");
+			return "home/error";
+		}
+
+		Patient existingUsername = patientService.findPatientByUsername(patient.getUsername());
+
+		if (existingUsername != null) {
+			model.addAttribute("errormsg", "Username already exists");
+			return "home/error";
+		}
+
+		int output = patientService.savePatient(patient);
+		
+		if (output > 0) {
+			return "redirect:/login";
+		} else {
+			model.addAttribute("errormsg", "Account creation failed");
+			return "home/error";
+		}
 	}
