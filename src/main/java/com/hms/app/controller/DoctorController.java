b053574848d27e11bddf243d26680901af65a4d8
SaiKiranNandipati <nandipatisaikiran3761@gmail.com>
2023-06-25 23:15:31 -0500

    Adding the Doctor controller class with cancle appointments,confirm appointment apis ..

--- src/main/java/com/hms/app/controller/DoctorController.java
+++ src/main/java/com/hms/app/controller/DoctorController.java
@@ -39,18 +39,47 @@ public class DoctorController {
 	private DoctorService doctorService;
 
 	@GetMapping("/doctor")
-	public String getPatientWelcomePage(Model model, HttpSession session)
-	{
+	public String getPatientWelcomePage(Model model, HttpSession session) {
 		@SuppressWarnings("unchecked")
-        List<String> messages = (List<String>) session.getAttribute("MY_SESSION_MESSAGES");
+		List<String> messages = (List<String>) session.getAttribute("MY_SESSION_MESSAGES");
 
-		if(messages == null) {
+		if (messages == null) {
 			model.addAttribute("errormsg", "Session Expired. Please Login Again");
 			return "home/error";
 		}
-        model.addAttribute("sessionMessages", messages);
+		model.addAttribute("sessionMessages", messages);
 		String email = messages.get(0);
-        
-		
+
 		return "doctor/welcomedoctor";
+	}
+
+	@GetMapping("/doctorappointments")
+	public String appointments(Model model, HttpSession session) {
+
+		@SuppressWarnings("unchecked")
+		List<String> messages = (List<String>) session.getAttribute("MY_SESSION_MESSAGES");
+
+		if (messages == null) {
+			model.addAttribute("errormsg", "Session Expired. Please Login Again");
+			return "home/error";
+		}
+		model.addAttribute("sessionMessages", messages);
+		String email = messages.get(0);
+		List<Appointment> appointments = doctorService.getAllDoctorAppointments(email);
+		model.addAttribute("appointments", appointments);
+		return "doctor/appointments";
+	}
+
+	@GetMapping("/cancelAppointments/{id}")
+	public String cancelAppointment(Model model, HttpSession session, @PathVariable(name = "id") Long id) {
+
+		patientService.cancelAppointment(id);
+		return "redirect:/doctor";
+	}
+
+	@GetMapping("/confirmAppointment/{id}")
+	public String confirmAppointment(Model model, HttpSession session, @PathVariable(name="id") Long id) {
+
+		doctorService.confirmAppointment(id);
+		return "redirect:/doctor";
 	}
